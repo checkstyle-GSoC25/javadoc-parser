c6125785b821c84a52bfa2ff88cb0066a9335b9c
Mohamed Mahfouz <mohamadmahfouz39@gmail.com>
2025-06-18 11:49:55 +0300

    Issue #12: Handle curly brackets inside code tag (#13)

--- src/test/java/AstTest.java
+++ src/test/java/AstTest.java
@@ -88,4 +88,10 @@ public class AstTest extends AbstractTestSupport {
     public void testNonTightTags() throws IOException {
         verifyAst(getPath("htmlNonTight1.txt"), getPath("htmlNonTight1.javadoc"));
     }
+
+
+    @Test
+    public void testCodeTagWithNestedBraces() throws IOException {
+        verifyAst(getPath("codeTagWithNestedBraces.txt"), getPath("codeTagWithNestedBraces.javadoc"));
+    }
 }
